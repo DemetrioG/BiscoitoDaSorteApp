e2cad230476fe86b3374223ca639da6fb6c3bb5f
DemetrioG <demetrio.gabriel@hotmail.com>
2021-09-17 21:48:10 -0300

    App biscoito da sorte

--- App.js
+++ App.js
@@ -1,39 +1,95 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
 
 export default class App extends Component {
 
   constructor(props){
     super(props)
     this.state = {
-      title: 'Opa'
+      showFrase: '',
+      img: require('./src/biscoito.png')
     }
 
-    this.entrar = this.entrar.bind(this)
+    this.frases = [
+      'A persistência é o caminho do êxito.',
+      'Motivação é a arte de fazer as pessoas fazerem o que você quer que elas façam porque elas o querem fazer.',
+      'Motivação é a arte de fazer as pessoas fazerem o que você quer que elas façam porque elas o querem fazer.',
+      'No meio da dificuldade encontra-se a oportunidade.',
+      'Lute. Acredite. Conquiste. Perca. Deseje. Espere. Alcance. Invada. Caia. Seja tudo o quiser ser, mas, acima de tudo, seja você sempre.',
+      'A verdadeira motivação vem de realização, desenvolvimento pessoal, satisfação no trabalho e reconhecimento.',
+      'Pedras no caminho? Eu guardo todas. Um dia vou construir um castelo.',
+      'É parte da cura o desejo de ser curado.'
+    ]
+
+    this.getFrase = this.getFrase.bind(this)
   }
 
-  entrar(title) {
-    this.setState({ title: title })
+  getFrase() {
+    let aleatorio = Math.floor(Math.random() * this.frases.length)
+    
+    this.setState({
+      showFrase: this.frases[aleatorio],
+      img: require('./src/biscoitoAberto.png')
+    })
   }
 
-  render() {
+  render(){
     return (
-      <View style={ styles.view }>
+      <View style={style.container}>
+        
+        <Image source={this.state.img} style={style.img}/>
 
-        <Text>Texto 1</Text>
-        <Text>Texto 2</Text>
-        <Text>Texto 3</Text>
-        <Text>Texto 4</Text>
+        <Text style={style.textFrase}>{this.state.showFrase}</Text>
+
+        <TouchableOpacity style={style.btn} onPress={this.getFrase}>
+          <View style={style.btnArea}>
+            <Text style={style.btnText}>Quebrar biscoito</Text>
+          </View>
+        </TouchableOpacity>
 
       </View>
     )
   }
 }
 
-const styles = StyleSheet.create({
-  view: {
-    marginTop: 30,
-    borderWidth: 1,
-    borderColor: 'red'
+const style = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    marginTop: 20,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  img: {
+    width: 250,
+    height: 250
+  },
+
+  textFrase: {
+    fontSize: 20,
+    textAlign: 'center',
+    padding: 15
+  },
+
+  btn: {
+    width: 200,
+    height: 50,
+    backgroundColor: '#F29C41',
+    borderRadius: 20,
+    padding: 10,
+    marginTop: 80
+  },
+
+  btnArea: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  btnText: {
+    fontSize: 16,
+    fontWeight: '700'
   }
 })
